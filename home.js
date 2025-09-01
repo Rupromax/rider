b4fedcc429f0743c3d0560c7dffccfdba06c3bc4
Ru <andy9729701@gmail.com>
2025-09-01 22:19:05 +0800

    手機

--- home.js
+++ home.js
@@ -1,11 +1,7 @@
-let menu=document.querySelector("#menu-bar");
-let navbar=document.querySelector(".navbar");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
 // 按下按鈕之後產生的變化
-menu.onclick =() =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
 if (menu && navbar) {
     menu.onclick = () => {
         menu.classList.toggle('fa-times');
